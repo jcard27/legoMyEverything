1164678677e5006bac413b100517cf91e67595c6
jcard27 <jenniferleecardona@gmail.com>
2020-10-10 11:23:08 -0700

    playing with quantization using kmeans

--- main.m
+++ main.m
@@ -6,6 +6,14 @@ clear all
 %% INPUTS:
 % filename = 'images/apple.jpg'; %path to image file
 filename = 'images/rainbow.jpg';
+% filename = 'images/fish.jpg';
+% filename = 'images/mattAndTurtle.png';
+% filename = 'images/jenAndMatt.jpeg';
+% filename = 'images/thermal.png';
+% filename = 'images/pika.jpg';
+% filename = 'images/bulbasaur.png';
+% filename = 'images/pika2.jpeg';
+% filename = 'images/katamari.jpeg';
 
 width = 50; %width of image in legos
 
@@ -15,27 +23,62 @@ figure
 imshow(im)
 title('Original')
 
-%% Enhance Color
-%Couldn't get something working for this yet. HSV conversion was closest
-%but ended up with negative pixel values I couldn't deal with
+%% Quantize image with kmeans
 
+%Unwrap image
+w = size(im, 1);
+h = size(im, 2);
+unwrapped_im = zeros(w*h, 3);
+count = 0;
+for ii = 1:w
+    for jj = 1:h
+        count = count + 1;
+        unwrapped_im(count, :) = im(ii, jj, :);
+    end
+end
+
+%Find clusters and re-map to image shape
+numClusters = 10;
+clusters = kmeans(unwrapped_im, numClusters);
+cluster_mat = zeros(w, h);
+count = 0;
+for ii = 1:w
+    for jj = 1:h
+        count = count + 1;
+        cluster_mat(ii, jj) = clusters(count);
+    end
+end
+
+%Replace rgb values with mean for cluster
+r = im(:, :, 1);
+g = im(:, :, 2);
+b = im(:, :, 3);
+for ii = 1:numClusters
+    [row, c] = find(cluster_mat == ii);
+    r(cluster_mat == ii) = mean(r(cluster_mat == ii));
+    g(cluster_mat == ii) = mean(g(cluster_mat == ii));
+    b(cluster_mat == ii) = mean(b(cluster_mat == ii));
+end
+
+newIm = zeros(size(im));
+newIm(:, :, 1) = r;
+newIm(:, :, 2) = g;
+newIm(:, :, 3) = b;
+newIm = newIm./255;
+im = newIm;
+
+figure
+imshow(newIm)
+title('Quantized with Kmeans')
+
+%% Enhance Color
+%This is still experimental
+% 
 % origmax = max(im(:))./255;
 % hsv = rgb2hsv(im);
 % hsv(:, :, 2) = hsv(:, :, 2) * 1.5;
 % im = hsv2rgb(hsv);
-% 
-    % 
-% minval = min(im(:));
-% maxval = max(im(:));
-% pxRange = maxval-minval;
-% im = (single(im) - minval).*(single(origmax)./pxRange);
-
-% im = single(im).*1.2./255;
-% im(im > 1) = 1;
-% 
-% figure
-% imshow(im)
-% title('Color Enhanced Image')
+% im(im < 0) = 0;
 
 
 %% Rescale based on desired width
@@ -44,6 +87,7 @@ im = imresize(im, scale);
 figure
 imshow(im, 'InitialMagnification',5000)
 title('Resized Image')
+
 
 %% Recolor based on available lego colors
 
@@ -62,9 +106,10 @@ colors = [0, 61, 165;... %Bright Blue
           162, 170, 173;... %Medium Stone Grey
           122, 62, 58;...%Reddish Brown
           51	0	114;...Medium Lilac
-          152	29	151;...%Light Purple
+          241	167	220;...%Light Purple
           49	38	29;...%Black
           181	189	0;...%Bright Yellowish Green
+          255	205	0;...%Bright Yellow
           91	103	112;...%Dark Stone Grey
           217	217	214]./255;%White         
 
@@ -81,13 +126,14 @@ legoElementID = [614123;...%Bright Blue
                 4517996;...%Light Purple
                 614126;...%Black
                 4183133;...%Bright Yellowish Green
+                614124;...%Bright Yellow
                 4210633;...%Dark Stone Grey
                 614101];%White
       
 %Find nearest lego for each pixel
 numColors = size(colors,1);
 distance = zeros(numColors, 1);
-im = single(im)./255;
+% im = single(im)./255;
 for ii = 1:size(im,1)
     for jj = 1:size(im,2)
         im_color = squeeze((im(ii, jj, :)))';
